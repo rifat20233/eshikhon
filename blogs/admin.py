dc94a00473ff4af587118cc361dfe49fc2f1862a
rifat20233 <alfarifat2023@gmail.com>
2023-11-17 22:59:12 +0600

    models to admin accces

--- blogs/admin.py
+++ blogs/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Blogtable
 # Register your models here.
+
+class ShowFields(admin.ModelAdmin):
+    list_display = ('id','subject','content')
+    search_fields = ('subject',)
+
+admin.site.register(Blogtable,ShowFields)
